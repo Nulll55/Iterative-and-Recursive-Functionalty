57c1409aeb2f4b850f8981772a9218e01d9a7003
username <useremail>
2025-04-19 21:45:56 -0700

    First draft

--- Iterative_and_Recursive_Functionalty.py
+++ Iterative_and_Recursive_Functionalty.py
@@ -1,1 +1,23 @@
+# 4! = 1*2*3*4 = 24
+# Factorial using an iterative function
+
+# numbers = 0, 5, 10, 25, 50, 100
+
+print("factorial results using an interative function")
+
+Numbers = [0, 5, 10, 25, 50, 100]
+
+for n in Numbers
+    result = 1
+    for i in range(1, n + 1):
+        result *=i
+    print(F"{n}! = {results}")
+
+# Recursive factorial calculation 
+
+
+
+
+
+
 
