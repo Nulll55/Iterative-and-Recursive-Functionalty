68d15d8c6fda0e30304d48f403189d453f2c8891
username <useremail>
2025-04-20 19:52:10 -0700

    final version CIS261 lab 2.2

--- Iterative_and_Recursive_Functionalty.py
+++ Iterative_and_Recursive_Functionalty.py
@@ -1,19 +1,27 @@
 # 4! = 1*2*3*4 = 24
 # Factorial using an iterative function
-
-# numbers = 0, 5, 10, 25, 50, 100
-
 print("factorial results using an interative function")
 
-Numbers = [0, 5, 10, 25, 50, 100]
+numbers = [0, 5, 10, 25, 50, 100]
 
-for n in Numbers
+for n in numbers:
     result = 1
     for i in range(1, n + 1):
         result *=i
-    print(F"{n}! = {results}")
+    print(F"{n}! = {result}")
 
 # Recursive factorial calculation 
+def factorial_recursive (n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial_recursive (n - 1)
+    
+for n in numbers:
+    print(f"{n}! = {factorial_recursive(n)}")
+
+
+
 
 
 
